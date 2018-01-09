e88523d995916667e6cf35517a7ed50ea30f21f8
rafael_chicoli <rafael_chicoli@regis24.de>
2018-01-09 21:34:48 +0100

    driver: read logfile and send to logstash

--- pkg/docker/driver.go
+++ pkg/docker/driver.go
@@ -31,7 +31,7 @@ const (
 	name = "logstashlog"
 )
 
-type LoggerInfo struct {
+type Inspect struct {
 	Config              map[string]string `json:"config,omitempty"`
 	ContainerID         string            `json:"containerID"`
 	ContainerName       string            `json:"containerName"`
@@ -47,17 +47,24 @@ type LoggerInfo struct {
 }
 
 type Driver struct {
-	mu     sync.Mutex
-	logs   map[string]*logPair
+	mu   sync.Mutex
+	logs map[string]*container
+
 	logger logger.Logger
 
 	client *logstash.Logstash
+
+	file File
 }
 
-type logPair struct {
-	file   *os.File
+type container struct {
 	stream io.ReadCloser
 	info   logger.Info
+}
+
+type File struct {
+	fd     *os.File
+	rename bool
 }
 
 type LogMessage struct {
@@ -73,32 +80,40 @@ type LogMessage struct {
 	// be missing, gibberish, or nil)
 	Err error `json:"err,omitempty"`
 
-	LoggerInfo
+	Inspect
 	LogLine string `json:"message"`
 }
 
 func NewDriver() *Driver {
 
 	return &Driver{
-		logs: make(map[string]*logPair),
+		logs: make(map[string]*container),
 	}
 }
 
-func (d *Driver) StartLogging(file string, info logger.Info) error {
+func (d *Driver) StartLogging(pipe string, info logger.Info) error {
 
 	// get config from log-opt and validate it
 	cfg := defaultLogOpt()
 	if err := cfg.validateLogOpt(info.Config); err != nil {
 		return errors.Wrapf(err, "error: logstash-options: %q", err)
 	}
-	// logrus.WithField("id", info.ContainerID).Debugf("log-opt: %v", cfg)
 
 	d.mu.Lock()
-	if _, exists := d.logs[file]; exists {
+	if _, exists := d.logs[pipe]; exists {
 		d.mu.Unlock()
-		return fmt.Errorf("logger for %q already exists", file)
+		return fmt.Errorf("logger for %q already exists", pipe)
 	}
 	d.mu.Unlock()
+
+	ctx := context.Background()
+
+	// pipe file for streaming the logs
+	logrus.WithField("id", info.ContainerID).WithField("pipe", pipe).WithField("logpath", info.LogPath).Debugf("Start logging")
+	fifo, err := fifo.OpenFifo(ctx, pipe, syscall.O_RDONLY, 0700)
+	if err != nil {
+		return errors.Wrapf(err, "error opening logger fifo: %q", fifo)
+	}
 
 	// cache messages to the filesystem, if the target service is not responding
 	if info.LogPath == "" {
@@ -107,26 +122,18 @@ func (d *Driver) StartLogging(file string, info logger.Info) error {
 	if err := os.MkdirAll(filepath.Dir(info.LogPath), 0755); err != nil {
 		return errors.Wrap(err, "error setting up logger dir")
 	}
-	l, err := os.OpenFile(info.LogPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0640)
+	f, err := d.openLogFile(info.LogPath)
 	if err != nil {
-		return errors.Wrapf(err, "error create cache  log file: %q", l)
+		return errors.Wrapf(err, "error create cache  log file: %q", f)
 	}
-
-	ctx := context.Background()
-
-	logrus.WithField("id", info.ContainerID).WithField("file", file).WithField("logpath", info.LogPath).Debugf("Start logging")
-	f, err := fifo.OpenFifo(ctx, file, syscall.O_RDONLY, 0700)
-	if err != nil {
-		return errors.Wrapf(err, "error opening logger fifo: %q", file)
-	}
+	d.file.fd = f
 
 	d.mu.Lock()
-	lf := &logPair{
-		file:   l,
-		stream: f,
+	lf := &container{
+		stream: fifo,
 		info:   info,
 	}
-	d.logs[file] = lf
+	d.logs[pipe] = lf
 	d.mu.Unlock()
 
 	d.client, err = logstash.NewClient(cfg.scheme, cfg.host, cfg.port, cfg.timeout)
@@ -139,7 +146,18 @@ func (d *Driver) StartLogging(file string, info logger.Info) error {
 	return nil
 }
 
-func (d *Driver) consumeLog(lf *logPair) {
+func (d *Driver) openLogFile(file string) (*os.File, error) {
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0640)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error create cache  log file: %q", f)
+	}
+
+	d.file.rename = false
+
+	return f, nil
+}
+
+func (d *Driver) consumeLog(lf *container) {
 
 	dec := protoio.NewUint32DelimitedReader(lf.stream, binary.BigEndian, 1e6)
 	defer dec.Close()
@@ -147,7 +165,7 @@ func (d *Driver) consumeLog(lf *logPair) {
 	var buf logdriver.LogEntry
 	var msg LogMessage
 
-	writer := bufio.NewWriter(lf.file)
+	cache := bufio.NewWriter(d.file.fd)
 
 	for {
 		if err := dec.ReadMsg(&buf); err != nil {
@@ -180,29 +198,23 @@ func (d *Driver) consumeLog(lf *logPair) {
 
 		m, err := json.Marshal(msg)
 		if err != nil {
-			logrus.WithField("id", lf.info.ContainerID).
-				WithError(err).
-				WithField("message", msg).
-				Error("error unmarshaling json log message")
+			logrus.WithField("id", lf.info.ContainerID).WithError(err).WithField("message", msg).Error("error unmarshaling json log message")
 		}
 		m = append(m, '\n')
 
+		// TODO: create a separeted gorouting for sending log messages to logstash
 		if err := d.client.Write(m); err != nil {
-			logrus.WithField("id", lf.info.ContainerID).
-				WithError(err).
-				WithField("message", msg).
-				Error("error sending log message to logstash")
+			logrus.WithField("id", lf.info.ContainerID).WithError(err).WithField("message", msg).Error("error sending log message to logstash")
 
 			// cache log messages temporary to logfile
-			if _, err := writer.Write(m); err != nil {
+			if _, err := cache.Write(m); err != nil {
 				logrus.WithField("id", lf.info.ContainerID).WithError(err).WithField("message", msg).Error("error writing log message")
 			}
-			if err := writer.Flush(); err != nil {
-				logrus.WithField("id", lf.info.ContainerID).
-					WithError(err).
-					WithField("message", msg).
-					Error("error flush log message")
+			if err := cache.Flush(); err != nil {
+				logrus.WithField("id", lf.info.ContainerID).WithError(err).WithField("message", msg).Error("error flush log message")
 			}
+			go d.renameFile(lf.info.LogPath)
+
 			// continue // do we need this?
 		}
 
@@ -210,29 +222,90 @@ func (d *Driver) consumeLog(lf *logPair) {
 	}
 }
 
-func (d *Driver) StopLogging(file string) error {
-	logrus.WithField("file", file).Debugf("Stop logging")
+func (d *Driver) renameFile(file string) {
+
+	// avoid starting multiple go routines
+	if d.file.rename {
+		return
+	}
+
+	d.file.rename = true
+
+	timestamp := time.Now().Format(time.RFC3339Nano)
+
+	for {
+		// only rename file, if client is send messages to logstash
+		if !d.client.Reconnecting() {
+
+			// rename the file with an timestamp attached to it
+			if err := os.Rename(file, file+timestamp); err != nil {
+				logrus.WithField("file", file).WithError(err).WithField("file", file).Error("moving file")
+			}
+
+			f, err := d.openLogFile(file)
+			if err != nil {
+				logrus.WithField("file", file).WithError(err).WithField("file", file).Error("moving file")
+				// TODO: this is bad, if no new logfile can be created
+				// maybe add a retry or something
+				return
+			}
+			d.file.fd = f
+
+			go d.readLogFile(f)
+			break
+		}
+		time.Sleep(time.Second * 1)
+	}
+}
+func (d *Driver) readLogFile(file *os.File) {
+
+	reader := bufio.NewReader(file)
+
+	// TODO: read file by chunk
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			logrus.WithField("file", file).WithError(err).WithField("line", line).Error("error: reading cache log file")
+			continue
+		}
+		if err := d.client.Write(line); err != nil {
+			logrus.WithField("id", "todo").WithError(err).Error("error: sending log message to logstash")
+		}
+	}
+
+	logrus.WithField("id", "todo").WithField("file", file.Name()).Debugf("debug: removing logfile")
+	if err := os.Remove(file.Name()); err != nil {
+		logrus.WithField("id", "todo").WithError(err).Error("error: removing logfile")
+	}
+
+}
+
+func (d *Driver) StopLogging(pipe string) error {
+	logrus.WithField("pipe", pipe).Debugf("Stop logging")
 
 	d.mu.Lock()
-	lf, ok := d.logs[file]
+	lf, ok := d.logs[pipe]
 	if ok {
-		// close log file
-		d.logs[file].file.Close()
 
 		// close fifo
 		lf.stream.Close()
-		delete(d.logs, file)
+		delete(d.logs, pipe)
 	}
 	d.mu.Unlock()
+
+	// close log file descriptor
+	if d.file.fd != nil {
+		d.file.fd.Close()
+	}
 
 	// close connection, if still open
 	err := d.client.Close()
 	if err != nil {
 		return err
 	}
-
-	//TODO: log files, only if empty
-	// delete only rootfs
 
 	return nil
 }
